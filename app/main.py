181c44bb062484cfa62a4d86ba1eef3367804e1e
Kamilovkz <001kamilov@gmail.com>
2025-06-26 00:33:03 +0500

    add two simple endpoints for books

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI, UploadFile, File
+import json
 
 app = FastAPI()
+
+with open("app/data/books.json", "r") as f:
+    books = json.load(f)
+
+
+@app.get("/books/")
+def list_books():
+    return [{"id": b["id"], "title": b["title"], "author": b["author"]} for b in books]
+
+@app.get("/books/{book_id}")
+def get_book(book_id: int):
+    for book in books:
+        if book["id"] == book_id:
+            return book
+    return {"error": "Book not found"} 
 
 @app.post("upload-book/")
 async def upload_book(file: UploadFile = File(...)):
